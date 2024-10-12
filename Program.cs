ce1527aecd3b46bb6d43c42ea101aa2c463845de
Geethu31 <sgeethunair12@gmail.com>
2024-10-12 10:41:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //16
+            //Console.WriteLine("Enter the first number:");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //if (num1 == num2)
+            //    Console.WriteLine("True");
+            //else
+            //    Console.WriteLine("False");
+            //Console.ReadLine();
+
+
+            //15
+            //int number1 = 5;
+            //int number2 = 3;
+            //int sum = number1 + number2;
+            //Console.WriteLine($"The sum is {sum}");
+            //Console.ReadLine();
+
+
+            //14
+            //Console.WriteLine("Enter employee's name:"); // input employee's name
+            //string name = Console.ReadLine();            
+            //Console.WriteLine("Enter your age:");        // input age
+            //int age = Convert.ToInt32(Console.ReadLine());  
+            //Console.WriteLine("Enter your monthly salary:");  //input monthly salary
+            //double salary = Convert.ToInt32(Console.ReadLine());  
+            //Console.WriteLine($"Employee's name is {name} and age is {age} and monthly salary is {salary}"); //displaying all
+            //Console.ReadLine();
+
+
+            //13
+            //Console.WriteLine("Enter the height:");
+            //double height = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the width:");
+            //double width = Convert.ToInt32(Console.ReadLine());
+            //double areaOfRectangle = height * width;
+            //double areaInInches = areaOfRectangle * 0.393701;
+            //Console.WriteLine($"Area of rectangle in centimeters is {areaOfRectangle} and area of rectangle in inches {areaInInches}");
+            //Console.ReadLine();
+
+
+            //12
+            //Console.WriteLine("Enter the radius:");
+            //double radius = Convert.ToInt32(Console.ReadLine());
+            //const double PI = 3.14159;
+            //const int DAYS_IN_WEEK = 7;
+            //double circumference = 2 * PI * radius;
+            //Console.WriteLine($"Circumference is {circumference} and days in a week are {DAYS_IN_WEEK}");
+            //Console.ReadLine();
+
+
+            //11
+            //Console.WriteLine("Enter the radius:");
+            //double radius = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the length:");
+            //double length = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the width:");
+            //double width = Convert.ToInt32(Console.ReadLine());
+            //const double PI = 3.14;
+            //double areaOfCircle = PI * radius * radius;
+            //Console.WriteLine($"Area of circle is {areaOfCircle}");
+            //double areaOfRectangle = length * width;
+            //Console.WriteLine($"Area of rectangle is {areaOfRectangle}");
+            //Console.ReadLine();
+
+
+            //10
+            //int num1 = 5;
+            //Console.WriteLine($"Int is {num1}");
+            //double num2 = 0.5;
+            //    Console.WriteLine($"Double is {num2}");
+            //float fnum = 62;
+            //Console.WriteLine($"Float is {fnum}");
+            //char letter = 'A';
+            //Console.WriteLine($"Char is {letter}");
+            //string word = "Hello";
+            //    Console.WriteLine($"String is {word}");
+            //bool my = true;
+            //    Console.WriteLine($"Bool is {my}");
+            //Console.ReadLine();
+
+            //9
+            //Console.WriteLine("Enter the weight");
+            //double weight = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the height");
+            //double height = double.Parse(Console.ReadLine());
+            //double BMI = weight / (height * height);
+            //Console.WriteLine($"The BMI is {BMI}");
+            //Console.ReadLine();
+
+
+
+            //8
+            //Console.WriteLine("Enter the string");
+            //String input = Console.ReadLine();
+            //int characters = input.Replace(" ","").Length;
+            //Console.WriteLine($"number of characters is {characters}");
+            //Console.ReadLine();
+
+
+            //7
+            //Console.WriteLine("Enter the seconds");
+            //int sec = Convert.ToInt32(Console.ReadLine());
+            //int min = (sec %3600)/60;
+            //int hour = sec / 3600;
+            //Console.WriteLine($"In seconds: {sec}");
+            //Console.WriteLine($"In minutes: {min}");
+            //Console.WriteLine($"In hour: {hour}");
+            //Console.ReadLine();
+
+            //6
+            //Console.WriteLine("Enter the radius");
+            //double radius = Convert.ToInt32(Console.ReadLine());
+            //const double PI = 3.14;
+            //double area = PI * radius * radius;
+            //Console.WriteLine($"Area is {area}");
+            //Console.ReadLine();
+
+
+            //5
+            //Console.WriteLine("Enter the principal Amount");
+            //double principalAmount = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the rate of interest");
+            //double rateOfInterest = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the years");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //double simpleInterest = principalAmount * rateOfInterest * year;
+            //Console.WriteLine($"Simple interest is {simpleInterest}");
+            //Console.ReadLine();
+
+            //4
+            //Console.WriteLine("Enter the temperature:");
+            //double temp = Convert.ToInt32(Console.ReadLine());
+            //double fah = 0;
+            //fah = (temp * 9 / 5)+ 32;
+            //Console.WriteLine($"Temperature is {fah}");
+            //Console.ReadLine();
+
+
+            //3
+            //Console.WriteLine("Enter the first number:");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the second number:");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //int sum, diff, mult, div;
+            //sum = num1 + num2;
+            //diff = num1 - num2;
+            //mult = num1 * num2;
+            //Console.WriteLine($"Sum is {sum} and difference is {diff} and multiplication is {mult}");
+            //if (num2 != 0)
+            //{
+            //div = num1 / num2;
+            //Console.WriteLine($"Division is {div}");
+            //}
+            //else
+            //{
+            //Console.WriteLine($"Division is not possible");
+            //Console.ReadLine();
+            //}
+
+
+            //2
+            // Console.WriteLine("Enter your full name:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Enter your age:");
+            //string age = Console.ReadLine();
+            //Console.WriteLine($"Hello! {name} You are {age} years old");
+            //Console.WriteLine("Enter you favourite color");
+            //string color = Console.ReadLine();
+            //Console.WriteLine($"Hello! {name} You are {age} years old and favourite color is {color} ");
+            //Console.ReadLine();
+
+            //1
+            //Console.WriteLine("Hello World");
+            //Console.WriteLine("Enter the name:");
+            //string username = Console.ReadLine();
+            //Console.WriteLine("Have a nice day!" +username);
+            // Console.ReadLine();
+
+
+        }
+    }
+}
